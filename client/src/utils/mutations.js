1f9af8fd481e3bcae80cef8fe6e39d56de7bf19d
Emily Hill <emhillmed2021@gmail.com>
2023-01-09 16:52:09 -0600

    created mutations.js and added logic, no idea if correct

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,46 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($id: ID!, $authors: [String]!, $title: String, $description: String, $image: String, $link: String) {
+    saveBook(bookId: $id, authors: $authors, title: $title, description: $desciption, image: $image, link: $link) {
+      bookId
+      authors
+      title
+      description
+      image
+      link
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($id: ID!) {
+    removeBook(bookId: $id) {
+      bookId
+    }
+  }
+`;
